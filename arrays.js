5b83d4149bf43492c4a7b8b2267530aa1fdd1286
astronomical-queue-4656 <halierae17@outlook.com>
2020-11-12 04:09:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,4 +29,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   chocolateBars.pop();
   return chocolateBars;
 }
-function removeElementFromEndOfArray()
+function removeElementFromEndOfArray(chocolateBars) {
+  
+}
